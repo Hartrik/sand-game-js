f949c2013b3d95be4097da88d53c921bf2991e1b
Patrik Harag <patrikharag@gmail.com>
2023-08-17 21:56:58 +0200

    Minified builds

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import image from '@rollup/plugin-image';
 import { string } from "rollup-plugin-string";
+import terser from '@rollup/plugin-terser';
 import pkg from './package.json';
 
 export default [
@@ -24,6 +25,30 @@ export default [
                 include: "**/*.svg",
                 exclude: []
             })
+        ]
+    },
+
+    // browser-friendly UMD build, MINIMIZED
+    {
+        input: 'src/main.js',
+        output: {
+            name: 'SandGameJS',
+            file: pkg.browser_min,
+            format: 'umd'
+        },
+        plugins: [
+            resolve(), // so Rollup can find libraries
+            commonjs(), // so Rollup can convert libraries to an ES modules
+            image({
+                include: "**/*.png",
+                exclude: []
+            }),
+            string({
+                include: "**/*.svg",
+                exclude: []
+            }),
+
+            terser()
         ]
     }
 ];
