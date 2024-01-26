a0bee56baefb0ccc9dd392a9448b2e8cd177d19e
Patrik Harag <patrikharag@gmail.com>
2024-01-26 23:39:12 +0100

    Add predicates

--- src/def/PredicateDefs.js
+++ src/def/PredicateDefs.js
@@ -3,7 +3,7 @@ import {ElementHead} from "../core/ElementHead";
 /**
  *
  * @author Patrik Harag
- * @version 2024-01-10
+ * @version 2024-01-26
  */
 export class PredicateDefs {
 
@@ -53,5 +53,44 @@ export class PredicateDefs {
     static IS_FISH_PART = function (elementHead, elementTail) {
         const behaviour = ElementHead.getBehaviour(elementHead);
         return behaviour === ElementHead.BEHAVIOUR_FISH || behaviour === ElementHead.BEHAVIOUR_FISH_BODY;
+    };
+
+    /** @type {function(elementHead:number, elementTail:number):boolean} */
+    static IS_SAND = function (elementHead, elementTail) {
+        if (PredicateDefs.IS_POWDER(elementHead, elementTail)) {
+            return ElementHead.getBehaviour(elementHead) === 0
+                && ElementHead.getTypeModifierPowderMomentum(elementHead) === 6;
+        }
+        return false;
+    };
+
+    /** @type {function(elementHead:number, elementTail:number):boolean} */
+    static IS_SOIL = function (elementHead, elementTail) {
+        if (PredicateDefs.IS_POWDER(elementHead, elementTail)) {
+            const behaviour = ElementHead.getBehaviour(elementHead);
+            return behaviour === ElementHead.BEHAVIOUR_SOIL;
+        }
+        return false;
+    };
+
+    /** @type {function(elementHead:number, elementTail:number):boolean} */
+    static IS_GRAVEL = function (elementHead, elementTail) {
+        if (PredicateDefs.IS_POWDER(elementHead, elementTail)) {
+            return ElementHead.getBehaviour(elementHead) === 0
+                && ElementHead.getTypeModifierPowderMomentum(elementHead) === 3;
+        }
+        return false;
+    };
+
+    /** @type {function(elementHead:number, elementTail:number):boolean} */
+    static IS_BIOMASS = function (elementHead, elementTail) {
+        const behaviour = ElementHead.getBehaviour(elementHead);
+        return behaviour === ElementHead.BEHAVIOUR_GRASS
+            || behaviour === ElementHead.BEHAVIOUR_TREE
+            || behaviour === ElementHead.BEHAVIOUR_TREE_LEAF
+            || behaviour === ElementHead.BEHAVIOUR_TREE_TRUNK
+            || behaviour === ElementHead.BEHAVIOUR_TREE_ROOT
+            || behaviour === ElementHead.BEHAVIOUR_FISH
+            || behaviour === ElementHead.BEHAVIOUR_FISH_BODY;
     };
 }
